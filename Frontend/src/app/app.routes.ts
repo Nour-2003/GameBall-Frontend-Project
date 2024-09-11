81510442127e31ee09e53d7e234a19c7646bf6ca
Nour-2003 <noureldeanh@gmail.com>
2024-09-11 15:16:03 +0300

    Home Page/Profile Done

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,8 @@
 import { Routes } from "@angular/router";
 import { HomepageComponent } from "./homepage/homepage.component";
-import { FreindsComponent } from "./freinds/freinds.component";
 import { ProfileComponent } from "./profile/profile.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
+import { FreindsComponent } from "./friends/freinds.component";
 
 export const routes: Routes = [
   { path: "", redirectTo: "home", pathMatch: "full" },
