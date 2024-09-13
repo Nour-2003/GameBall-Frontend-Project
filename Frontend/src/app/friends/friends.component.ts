fb570692496a9ec227a0ff4ee11ca29b4425af3c
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-09-14 01:28:50 +0300

    friends

--- Frontend/src/app/friends/friends.component.ts
+++ Frontend/src/app/friends/friends.component.ts
@@ -1,48 +1,55 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FriendCardComponent } from "../friend-card/friend-card.component";
+import { HttpClient } from '@angular/common/http';  // Import HttpClient
 import { NgFor } from "@angular/common";
-
+import {BASE_URL,headers}from '../util/app.constants';  // Import BASE_URL from app.constants.ts
 @Component({
   selector: "app-friends",
   standalone: true,
   imports: [FriendCardComponent, NgFor],
   templateUrl: "./friends.component.html",
-  styleUrl: "./friends.component.css",
+  styleUrls: ["./friends.component.css"],  // Fixed typo (styleUrls instead of styleUrl)
 })
-export class FriendsComponent {
+export class FriendsComponent implements OnInit {
   friendsList: {
     name: string;
     description: string;
     image: string;
     isFriend: boolean;
-  }[] = [
-    {
-      name: "John",
-      description: "Best friend",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png",
-      isFriend: true,
-    },
-    {
-      name: "Ali Khaled",
-      description: "Close friend",
-      image:
-        "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-      isFriend: false,
-    },
-    {
-      name: "Moaz Emad",
-      description: "Old friend",
-      image:
-        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png",
-      isFriend: true,
-    },
-    {
-      name: "Yasser",
-      description: "Childhood friend",
-      image:
-        "https://images.ctfassets.net/h6goo9gw1hh6/2sNZtFAWOdP1lmQ33VwRN3/24e953b920a9cd0ff2e1d587742a2472/1-intro-photo-final.jpg?w=1200&h=992&fl=progressive&q=70&fm=jpg",
-      isFriend: false,
-    },
-  ];
+  }[] = [];
+
+  myId = 1;  // Replace with the current user's ID or fetch it dynamically
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    // Fetch users and following data on component initialization
+    this.fetchUsersAndFriends();
+  }
+
+  fetchUsersAndFriends() {
+    // First, fetch all users
+    this.http.get<{ body: any[] }>(`${BASE_URL}/users`, { headers }).subscribe((usersResponse) => {
+      const users = usersResponse.body;
+
+      // Then, fetch the following list (friends)
+      this.http.get<{ body: any[] }>(`${BASE_URL}/follow-management/following/${this.myId}`, { headers }).subscribe((followingResponse) => {
+        const friends = followingResponse.body;
+
+        // Map the users and check if they are in the following list
+        this.friendsList = users
+          .filter(user => user.id !== this.myId)  // Skip if my_id == user_id
+          .map(user => {
+            const isFriend = friends.some(friend => friend.id === user.id);
+            
+            return {
+              name: user.name,
+              description: isFriend ? 'Friend' : 'Not a friend',
+              image: user.profileImageUrl || 'https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_640.png',  // Use default image if null
+              isFriend
+            };
+          });
+      });
+    });
+  }
 }
--- Frontend/src/app/util/app.constants.ts
+++ Frontend/src/app/util/app.constants.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'https://cda1-102-43-5-37.ngrok-free.app/api/v1';
+import { HttpHeaders } from "@angular/common/http";
+
+export const BASE_URL = 'https://0618-102-43-5-37.ngrok-free.app/api/v1';
+export const headers = { 'ngrok-skip-browser-warning': '69420' };
