bc7160493fc95c37d74f323b15658822a19c9e11
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-09-15 18:25:51 +0300

    profile nav

--- Frontend/src/app/navbar/navbar.component.ts
+++ Frontend/src/app/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { getUser, user } from '../util/app.constants';
 import { NgIf } from '@angular/common';
+import { Router } from '@angular/router'; 
+
 @Component({
   selector: 'app-navbar',
   standalone: true,
@@ -11,9 +13,14 @@ import { NgIf } from '@angular/common';
 export class NavbarComponent implements OnInit {
   image: any= null;
   user: any = null;
+  constructor(private router: Router) { }
   ngOnInit(): void {
     this.image = user?.profileImageUrl 
     this.user = getUser();
+  }
+
+  navigateToProfile() {
+    this.router.navigate(['profile']);
   }
 
 }
