ada2f52aed3a88503138adcd5740cd63240e427e
mo-ibrahim22 <mibrahimm002@gmail.com>
2024-09-15 20:04:13 +0300

    editing comment

--- Frontend/src/app/comment/comment.component.ts
+++ Frontend/src/app/comment/comment.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BASE_URL } from '../util/app.constants';
+import { BASE_URL, headers } from '../util/app.constants';
 import { NgIf } from '@angular/common';
-import { headers } from '../util/app.constants';
+import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf,FormsModule],
   templateUrl: './comment.component.html',
-  styleUrl: './comment.component.css',
+  styleUrls: ['./comment.component.css'],
 })
 export class CommentComponent {
   @Input() author: string = '';
@@ -18,13 +19,15 @@ export class CommentComponent {
   @Input() postId: number = 0;
   @Input() IsAuth: boolean = false;
 
+  isEditing: boolean = false; // Flag for edit mode
+  editedComment: string = ''; // Store edited content
+
   constructor(private http: HttpClient) {}
 
+  // Delete Comment Method
   deleteComment() {
-    console.log(this.author,this.comment,this.postId,this.commentId);
-    
     if (confirm("Are you sure you want to delete this comment?")) {
-      this.http.delete(`${BASE_URL}/posts/${this.postId}/comments`,{
+      this.http.delete(`${BASE_URL}/posts/${this.postId}/comments`, {
         headers,
         body: { commentId: this.commentId },
       }).subscribe({
@@ -36,5 +39,44 @@ export class CommentComponent {
         }
       });
     }
+  }
+
+  // Start Edit Mode
+  startEditing() {
+    this.isEditing = true;
+    this.editedComment = this.comment; // Load the current comment into the editor
+  }
+
+  // Update Comment Method
+  updateComment(updatedContent: string) {
+    const requestBody = {
+      commendtId: this.commentId,
+      content: updatedContent,
+    };
+
+    this.http.put(`${BASE_URL}/posts/${this.postId}/comments`, requestBody, { headers }).subscribe({
+      next: (response: any) => {
+        console.log("Comment updated successfully", response);
+        this.comment = updatedContent; // Update the local comment content
+        this.isEditing = false; // Exit edit mode
+      },
+      error: (error) => {
+        console.error("Error updating comment", error);
+      }
+    });
+  }
+
+  // Save the Edited Comment
+  saveEdit() {
+    if (!this.editedComment.trim()) {
+      console.error("Edited content is empty");
+      return;
+    }
+    this.updateComment(this.editedComment);
+  }
+
+  // Cancel Edit Mode
+  cancelEdit() {
+    this.isEditing = false;
   }
 }
